76726031ba66f92daefbeb19820516334da5bf44
sigajeff <87101012+sigajeff@users.noreply.github.com>
2022-03-15 16:11:48 -0300

    del assets on build, devServer Cors & Swiper Barba

--- webpack.config.js
+++ webpack.config.js
@@ -35,5 +35,10 @@ module.exports = {
         port: 3010,
         hot: true,
         open: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        }
     }
 };
